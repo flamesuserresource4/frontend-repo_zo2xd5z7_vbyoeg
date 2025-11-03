79c3885ed22a8bd5380fbde6eb533237cbe404c3
Flames <vibe-coder@flames.com>
2025-11-03 20:43:07 +0000

    Update project prj_zo2xd5z7 - 2025-11-03T20:43:06.858Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hero from './components/Hero.jsx';
-import ServerCityBelt from './components/ServerCityBelt.jsx';
+import TechBelt from './components/TechBelt.jsx';
 import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="min-h-screen bg-black text-white selection:bg-emerald-500/30 selection:text-white">
       <Hero />
-      <ServerCityBelt />
+      <TechBelt />
       <About />
       <Contact />
     </div>
